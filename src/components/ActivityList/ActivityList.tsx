3902955ff0591f5cb7288e6f38d139461016312c
John Doerfler <j5602396@protonmail.com>
2026-02-17 10:51:38 -0600

    misc cleanup, fixes

--- src/components/ActivityList/ActivityList.tsx
+++ src/components/ActivityList/ActivityList.tsx
@@ -3,44 +3,62 @@ import type { EntryListTableType } from '../../types';
 import type { TableProps } from 'antd';
 import { useEntriesStore } from '../../stores/entriesStore';
 
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+});
+
+const hrefStyle: React.CSSProperties = { 
+    maxWidth: 180,
+    display: 'inline-block',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap' 
+}
+              
+const columns: TableProps<EntryListTableType>['columns'] = [
+    {
+        title: 'Created',
+        dataIndex: 'createdAt',
+        key: 'createdAt',
+        render: (createdAt) => 
+            dateFormatter.format( new Date( createdAt )),
+        sorter: (a, b) => { 
+            const timeA = new Date(a.createdAt).getTime();
+            const timeB = new Date(b.createdAt).getTime();
+            return (isNaN(timeA) ? 0 : timeA) - (isNaN(timeB) ? 0 : timeB);
+        },
+        defaultSortOrder: 'descend',    
+        sortDirections: ['ascend', 'descend'],
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',  
+    },
+    {
+        title: 'URL',
+        dataIndex: 'url',
+        key: 'url',
+        render: (url) => 
+            <a style={ hrefStyle } 
+                href={url} 
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                {url}
+            </a> 
+    }
+];
+
 const ActivityList: React.FC = () => {
     const entries = useEntriesStore( ( state ) => state.entries );
-
-    const columns: TableProps<EntryListTableType>['columns'] = [
-        {
-            title: 'Date',
-            dataIndex: 'date',
-            key: 'at',
-            render: (date) => 
-                new Intl.DateTimeFormat('en-US', {
-                    month: 'short',
-                    day: 'numeric'
-                }).format(date),
-            sorter: (a, b) => { 
-                const timeA = new Date(a.createdAt).getTime();
-                const timeB = new Date(b.createdAt).getTime();
-                return (isNaN(timeA) ? 0 : timeA) - (isNaN(timeB) ? 0 : timeB);
-            },
-            defaultSortOrder: 'descend',    
-            sortDirections: ['ascend', 'descend'],
-        },
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',  
-        },
-        {
-            title: 'URL',
-            dataIndex: 'url',
-            key: 'url',
-            render: (text) => <a href={text}>{text}</a> 
-        }
-    ];
 
     return (
         <Table<EntryListTableType> 
             columns={columns} 
             dataSource={entries} 
+            rowKey="key" 
         /> 
     )
 }  
